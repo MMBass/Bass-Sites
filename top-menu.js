27ad8e5ea2a4b38235f048a63b5e21ea69b682ca
unknown <mendibass@gmail.com>
2022-02-08 22:32:34 +0200

    first again

---
+++ top-menu.js
@@ -0,0 +1,13 @@
+function openNav() {
+  document.getElementById("BASS_SIDEBAR").style.display = "block";
+  document.getElementById("BASS_SIDEBAR").style.width = "100%";
+
+  // document.getElementById("main").style.marginLeft = "250px";
+}
+  
+function closeNav() {
+  document.getElementById("BASS_SIDEBAR").style.width = "0";
+  document.getElementById("BASS_SIDEBAR").style.display = "none";
+  // document.getElementById("main").style.marginLeft= "0";
+}
+
